4c2783da2a08c2d09ab0c64ea00505a62e9221fb
Piotr J. Tomaszewski <pjtomaszewski98@gmail.com>
2022-02-10 22:53:25 +0100

    Fix gh link & email

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,3 @@
-function decodeEmail() {
-    const email_element = document.getElementById("email");
-    const email_address = buf.toString(email_element.dataset.email);
-    email_element.setAttribute("href", "mailto:" + email_address);
-}
+const email_element = document.getElementById("email");
+const email_address = decodeURIComponent(escape(window.atob(email_element.dataset.email)));
+email_element.setAttribute("href", "mailto:" + email_address);
